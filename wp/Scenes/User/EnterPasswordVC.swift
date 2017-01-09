782a1fb1b9e13b37b8d9484ec806cf8ab8bd8655
chenshaochuan <136786658@qq.com>
2017-01-09 21:36:49 +0800

    我的关注界面UI和我的推单界面UI搭建

---
+++ wp/Scenes/User/EnterPasswordVC.swift
@@ -0,0 +1,69 @@
+//
+//  EnterPasswordVC.swift
+//  wp
+//
+//  Created by macbook air on 17/1/9.
+//  Copyright © 2017年 com.yundian. All rights reserved.
+//
+
+import UIKit
+
+class EnterPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        showTabBarWithAnimationDuration()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        hideTabBarWithAnimationDuration()
+        translucent(clear: false)
+    }
+    //MARK: -- 隐藏tabBar导航栏
+    func hideTabBarWithAnimationDuration() {
+        let tabBar = self.tabBarController?.tabBar
+        let parent = tabBar?.superview
+        let content = parent?.subviews[0]
+        let window = parent?.superview
+        
+        var tabFrame = tabBar?.frame
+        tabFrame?.origin.y = (window?.bounds)!.maxY
+        tabBar?.frame = tabFrame!
+        content?.frame = (window?.bounds)!
+    }
+    
+    func showTabBarWithAnimationDuration() {
+        let tabBar = self.tabBarController?.tabBar
+        let parent = tabBar?.superview
+        let content = parent?.subviews[0]
+        let window = parent?.superview
+        var tabFrame = tabBar?.frame
+        tabFrame?.origin.y = (window?.bounds)!.maxY - ((tabBar?.frame)?.height)!
+        tabBar?.frame = tabFrame!
+        
+        var contentFrame = content?.frame
+        contentFrame?.size.height -= (tabFrame?.size.height)!
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
