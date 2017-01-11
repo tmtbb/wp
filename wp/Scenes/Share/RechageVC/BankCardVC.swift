53fe0b534efa0c47fa85ee0fa30f8167211c26be
　buyiyang <776949529@qq.com>
2017-01-11 19:38:30 +0800

    我的银行卡测试接口界面重新调整

--- wp/Scenes/Share/RechageVC/BankCardVC.swift
+++ wp/Scenes/Share/RechageVC/BankCardVC.swift
@@ -1,115 +1,49 @@
 //
-//  BindingBankVC.swift
+//  BankCardVC.swift
 //  wp
 //
-//  Created by sum on 2017/1/6.
+//  Created by sum on 2017/1/11.
 //  Copyright © 2017年 com.yundian. All rights reserved.
 //
 
 import UIKit
-//  进入下个界面的key
-let pushFullbankInformation:String = "pushFullbankInformation"
-//银行卡视图 cell
 class BindingBankVCCell: OEZTableViewCell {
     // 银行名称
     @IBOutlet weak var bankName: UILabel!
-     // 银行名称
+    // 银行名称
     @IBOutlet weak var cardNum: UILabel!
     
     //
-//    var  Model = BankModel()
+    //    var  Model = BankModel()
     // 刷新cell
     override func update(_ data: Any!) {
         
-//       print(data)
-//
-//        bankName.text = model.name
-//        
-//        cardNum.text = "\(model.cardId)"
-        
-    
+        print(data)
     }
 }
-
-
 class BankCardVC: BaseListTableViewController {
-    
-    
-    var dataArry = [BankModel]()
+
     override func viewDidLoad() {
-        super.viewDidLoad()
         
-        title = "我的银行卡"
+        super.viewDidLoad()
+
         
     }
-    
-    //MARK:  网络请求
     override func didRequest() {
         AppAPIHelper.user().bankcardList(complete: { [weak self](result) -> ()? in
             
             let Model : BankListModel = result as! BankListModel
-            self?.didRequestComplete(Model.cardlist as AnyObject)
+//            self?.didRequestComplete(Model.cardlist as AnyObject)
             
-            self?.dataArry = Model.cardlist!
-                         print( Model)
-            self?.tableView.reloadData()
+//            self?.dataArry = Model.cardlist!
+            print( Model)
+//            self?.tableView.reloadData()
             return nil
             }, error: errorBlockFunc())
         
-    }
-    
-    //MARK:  添加银行卡
-    @IBAction func addBank(_ sender: Any) {
-        
-        self.performSegue(withIdentifier: pushFullbankInformation, sender: nil)
-        
         
     }
-   
-    
-    //MARK: 实现银行卡左滑删除的代理
-    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        
-        
-        let share = UITableViewRowAction(style: .normal, title: "删除") { action, index in
-            
-            
-//        let  model :BankModel = self.dataArry[indexPath.section] as BankModel
-            
-//          APISocketHelper.us
-            
-        }
-        share.backgroundColor = UIColor.red
-        
-        return [share]
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return dataArry.count
-    }
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat{
-        
-        return 15
-    }
-   
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        
-        return 1
-    }
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell : BindingBankVCCell = tableView.dequeueReusableCell(withIdentifier: "BindingBankVCCell") as! BindingBankVCCell
-        
 
+  
 
-        return cell
-        
-    }
-    
-    
-    
 }
