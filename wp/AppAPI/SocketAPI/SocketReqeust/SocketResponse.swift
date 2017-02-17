b5220909d88a78e2e91b66466d7e8d84a07f267c
yaowang <yaobanglin@163.com>
2017-02-17 17:51:58 +0800

    M*SS：解析json捕获异常

--- wp/AppAPI/SocketAPI/SocketReqeust/SocketResponse.swift
+++ wp/AppAPI/SocketAPI/SocketReqeust/SocketResponse.swift
@@ -52,7 +52,7 @@ class SocketJsonResponse: SocketResponse {
         if _jsonOjbect == nil  {
             
             do{
-                #if true
+                #if false
                  var json = String(data: body!.data!, encoding: .utf8)
                 json = json == nil ? "" : json;
                 debugPrint("\(body!.operate_code) \(body!.data_length) json\(json!)")
