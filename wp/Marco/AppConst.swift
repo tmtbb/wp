643718f8a95578152eb84caafce96fd649bbb45e
　buyiyang <776949529@qq.com>
2017-02-23 17:23:43 +0800

    修改充值历史

--- wp/Marco/AppConst.swift
+++ wp/Marco/AppConst.swift
@@ -67,9 +67,9 @@ class AppConst {
     class Network {
         #if true //是否测试环境
 
-        static let TcpServerIP:String = "61.147.114.78";
-        static let TcpServerPort:UInt16 = 30001;
-        static let TttpHostUrl:String = "http://61.147.114.78";
+        static let TcpServerIP:String = "61.147.114.87";
+        static let TcpServerPort:UInt16 = 16001;
+        static let TttpHostUrl:String = "http://61.147.114.87";
         //192.168.8.131
         #else
         static let TcpServerIP:String = "61.147.114.78";
