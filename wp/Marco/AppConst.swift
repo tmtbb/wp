5c6162d9d7209c806bb12ae5ff65474ac0443294
mu <1755731709@qq.com>
2017-05-02 08:31:52 +0800

    打开轮询开关

--- wp/Marco/AppConst.swift
+++ wp/Marco/AppConst.swift
@@ -23,7 +23,7 @@ class AppConst {
     static let DefaultPageSize = 15
     static let UMAppkey = "584a3eb345297d271600127e"
     static let isMock = false
-    static let isRepeate = false
+    static let isRepeate = true
     static let sha256Key = "t1@s#df!"
     static let pid = 1002
     static let klineCount: Double = 40
